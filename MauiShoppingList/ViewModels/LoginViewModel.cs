d50314f54ce7ac7377ba09e0d272fc3e157896e9
p c <muchenz@poczta.onet.pl>
2026-01-18 00:46:11 +0100

    add useBrowserForFacebokLogin for choose browser or webview

--- MauiShoppingList/ViewModels/LoginViewModel.cs
+++ MauiShoppingList/ViewModels/LoginViewModel.cs
@@ -17,8 +17,8 @@ using System.Diagnostics;
 
 namespace Test_MauiApp1.ViewModels
 {
-    public class LoginViewModel: BaseViewModel
-    {        
+    public class LoginViewModel : BaseViewModel
+    {
         private readonly UserService _userService;
         private readonly IConfiguration _configuration;
         private readonly StateService _stateService;
@@ -26,10 +26,10 @@ namespace Test_MauiApp1.ViewModels
 
         public Command LoginCommand { get; set; }
 
-        public LoginModel Model { get; set; } 
+        public LoginModel Model { get; set; }
 
-        
-        public LoginViewModel(UserService userService, IConfiguration configuration, StateService stateService, LoginService loginService,  string loginError=null)
+
+        public LoginViewModel(UserService userService, IConfiguration configuration, StateService stateService, LoginService loginService, string loginError = null)
         {
             LoginError = loginError;
             _stateService = stateService;
@@ -42,31 +42,31 @@ namespace Test_MauiApp1.ViewModels
 
             Model = new LoginModel();
             if (Preferences.Default.ContainsKey("UserName"))
-                Model.UserName = Preferences.Default.Get("UserName","");
+                Model.UserName = Preferences.Default.Get("UserName", "");
             if (Preferences.Default.ContainsKey("Password"))
-                Model.Password = Preferences.Default.Get("Password","");
+                Model.Password = Preferences.Default.Get("Password", "");
             Model.PropertyChanged += LoginViewModel_PropertyChanged;
 
         }
 
 
-        protected override async  Task OnAppearingAsync()
+        protected override async Task OnAppearingAsync()
         {
 
         }
 
         protected override async Task InitAsync()
         {
-           
+
 
         }
 
         private void LoginViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            if (e.PropertyName== "UserName")
-                  Preferences.Default.Set("UserName", Model.UserName);
+            if (e.PropertyName == "UserName")
+                Preferences.Default.Set("UserName", Model.UserName);
             if (e.PropertyName == "Password")
-                  Preferences.Default.Set("Password", Model.Password);
+                Preferences.Default.Set("Password", Model.Password);
 
         }
         private string _loginError;
@@ -82,10 +82,10 @@ namespace Test_MauiApp1.ViewModels
             if (!String.IsNullOrEmpty(Model.Password) && !String.IsNullOrEmpty(Model.UserName))
             {
                 LoginError = null;
-                
-                MessageAndStatusAndData<UserNameAndTokensResponse> response = 
+
+                MessageAndStatusAndData<UserNameAndTokensResponse> response =
                     await _loginService.LoginAsync(Model.UserName, Model.Password);
-                    
+
 
                 if (!response.IsError)
                 {
@@ -98,7 +98,7 @@ namespace Test_MauiApp1.ViewModels
                     //await Navigation.PushAsync(App.Container.Resolve<ListAggregationPage>());
                     // Navigation.RemovePage(Navigation.NavigationStack[^2]);
                 }
-                else 
+                else
                 {
 
                     LoginError = response.Message;
@@ -106,12 +106,13 @@ namespace Test_MauiApp1.ViewModels
             }
 
         }
-       
+
         public ICommand CreateAccountCommand
         {
             get
             {
-                return new Command(async (list) => {
+                return new Command(async (list) =>
+                {
 
                     await Navigation.PushAsync(App.Container.Resolve<RegistrationPage>());
 
@@ -121,78 +122,87 @@ namespace Test_MauiApp1.ViewModels
 
             }
         }
-        
+
         public ICommand LoginFacebookCommand
         {
             get
             {
-                return new Command(async (list) => {
+                return new Command(async (list) =>
+                {
 
 
-                    //------------------------ use browser ------------------------------------------------
+                    bool useBrowserForFacebokLogin = true;
 
-                    string _gid = string.Empty;
-
-                    if (!Preferences.Default.ContainsKey("gid"))
+                    if (useBrowserForFacebokLogin)
                     {
-                        _gid = Guid.NewGuid().ToString();
-                        Preferences.Default.Set("gid", _gid);
-                    }
-                    else
-                    {
-                        _gid = Preferences.Default.Get("gid", "");
-                    }
+                        //------------------------ use browser ------------------------------------------------
+
+                        string _gid = string.Empty;
+
+                        if (!Preferences.Default.ContainsKey("gid"))
+                        {
+                            _gid = Guid.NewGuid().ToString();
+                            Preferences.Default.Set("gid", _gid);
+                        }
+                        else
+                        {
+                            _gid = Preferences.Default.Get("gid", "");
+                        }
 
 
-                    string WebUrl = string.Format("https://www.facebook.com/v10.0/dialog/oauth?client_id={0}&response_type=code&redirect_uri={1}&state={2}&scope={3}",
-                     259675572518658,
-                        "https://192.168.0.222:5003/api/User",
-                     $"st=state123abc,ds=123456789, di={_gid}, returnUrl=fb259675572518658://authorize", "public_profile,email");
-                    
-                    Trace.WriteLine($"Facebook login id:!!!!!!!!!!!!!!!!!!!!!!!");
+                        string WebUrl = string.Format("https://www.facebook.com/v10.0/dialog/oauth?client_id={0}&response_type=code&redirect_uri={1}&state={2}&scope={3}",
+                         259675572518658,
+                            "https://192.168.0.222:5003/api/User",
+                         $"st=state123abc,ds=123456789, di={_gid}, returnUrl=fb259675572518658://authorize", "public_profile,email");
+
+                        Trace.WriteLine($"Facebook login id:!!!!!!!!!!!!!!!!!!!!!!!");
 
 
-                    var res = await WebAuthenticator.Default.AuthenticateAsync(
-                         new WebAuthenticatorOptions
-                         {   Url= new Uri(WebUrl),
-                             CallbackUrl= new Uri("fb259675572518658://authorize"),
-                             PrefersEphemeralWebBrowserSession = true
-                         });
-                
+                        var res = await WebAuthenticator.Default.AuthenticateAsync(
+                             new WebAuthenticatorOptions
+                             {
+                                 Url = new Uri(WebUrl),
+                                 CallbackUrl = new Uri("fb259675572518658://authorize"),
+                                 PrefersEphemeralWebBrowserSession = true
+                             });
 
-                    var isId = res.Properties.TryGetValue("id", out string id);
 
-                    var response = await _userService.GetTokensFromId(id);
+                        var isId = res.Properties.TryGetValue("id", out string id);
 
-                    if (response.IsError == false)
-                    {
+                        var response = await _userService.GetTokensFromId(id);
 
-                        _loginService.SetCredentials(response.Data.UserName, response.Data.Token, response.Data.RefreshToken);
+                        if (response.IsError == false)
+                        {
+
+                            _loginService.SetCredentials(response.Data.UserName, response.Data.Token, response.Data.RefreshToken);
 
                             App.Current.MainPage = new NavigationPage(App.Container.Resolve<ListAggregationPage>())
                             {
                                 BarBackgroundColor = Colors.WhiteSmoke,
                                 BarTextColor = Colors.Black
                             };
-                        
-                        //await Shell.Current.GoToAsync("//MainPage");
+
+                            //await Shell.Current.GoToAsync("//MainPage");
+                        }
+                        else
+                        {
+                            LoginError = response.Message;
+
+                        }
+
+
+                        Trace.WriteLine($"Facebook login id: {id}");
+
+
                     }
                     else
                     {
-                        LoginError = response.Message;
 
+                        //------------------------ use web view ------------------------------------------------
+
+                        await Navigation.PushAsync(App.Container.Resolve<LoginWebPage>());
                     }
 
-
-                    Trace.WriteLine($"Facebook login id: {id}");
-
-
-
-
-                    //-------------------- use web view  ----------------------------------------------------
-
-
-                    //await Navigation.PushAsync(App.Container.Resolve<LoginWebPage>());
 
                 });
 
