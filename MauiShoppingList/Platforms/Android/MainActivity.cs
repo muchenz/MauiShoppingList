4b8cfb428dea0fd3779b5d76ee4432d3b75360c5
p c <muchenz@poczta.onet.pl>
2026-01-17 22:30:59 +0100

    add WebAuthenticatorCallbackActivityForFacebook

--- MauiShoppingList/Platforms/Android/MainActivity.cs
+++ MauiShoppingList/Platforms/Android/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 
@@ -6,4 +7,17 @@ namespace Test_MauiApp1;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+}
+
+
+[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+[IntentFilter(
+    new[] { Intent.ActionView },
+    Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+    DataScheme = "fb259675572518658",   // Twój schemat URI
+    DataHost = "*"   // Twój host URI
+)]
+public class WebAuthenticatorCallbackActivityForFacebook : WebAuthenticatorCallbackActivity
+{
+
 }
